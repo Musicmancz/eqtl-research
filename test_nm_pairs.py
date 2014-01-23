3717c058225bdc6f6c3a1b8ce9e398fcd6d17992
musicmancz <charles.czysz@gmail.com>
2014-01-23 13:59:00 -0500

    test pairs script added to verify output of nomatch_pairs txt file

---
+++ test_nm_pairs.py
@@ -0,0 +1,32 @@
+import sys
+import pymysql
+import connect
+import random
+
+file = sys.argv[-1]
+
+[conn,cur] = connect.makeConn('ld')
+wrong = 0
+with open(file,'r') as f:
+  
+  for line in f:
+    if random.randrange(0,10) > 3:
+      continue
+
+    data = line.strip().split('\t')
+    print(data)
+
+    sql = "select %s from blocks where rsid='%s'"
+
+    cur.execute(sql % (data[0],data[1]))
+
+    result = cur.fetchall()
+
+    if int(result[0][0]) == int(data[2]):
+        pass
+    else:
+      wrong+=1
+
+
+
+print(wrong)
